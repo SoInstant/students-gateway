ec2cf9ef2ea275a712f6f2d9d59c2d4cd7b3a43f
Lohyc <lohyuchen12345@gmail.com>
2020-09-26 18:21:02 +0800

    some group, response implementation

--- helper.py
+++ helper.py
@@ -117,6 +117,11 @@ def get_posts(username: str, page: int, todo: int) -> list:
         post["author_name"] = author_name
         post["group_name"] = group_name
         post["viewed"] = username in post["viewed"]
+        response = None
+        for response_ in post["acknowledged"]:
+            if response_["username"] == username:
+                response = response_["response"]
+        post["acknowledged"] = response
 
         del post["author_id"], post["group_id"]
     return user_posts
